ee0995ad3af6f7a2922d5a5bcce29c8a64a9c29b
sourob2356 <sourob2356@gmail.com>
2026-06-19 18:51:36 +0600

    added admin dashboard

---
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import AdminDashboard from "@/components/admin/dashboard/Dashboard";
+
+const page = () => {
+  return <AdminDashboard />;
+};
+
+export default page;
--- src/constants/public/user.ts
+++ src/constants/public/user.ts
@@ -30,4 +30,4 @@ export const admin: User = {
   updatedAt: "2026-06-15T10:00:00.000Z",
 };
 
-export const activatedDashboard = doctor;
+export const activatedDashboard = admin;
